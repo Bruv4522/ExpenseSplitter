847d37137b7f8bf81d3e09d4697107f16c9931d7
BluesySugar4522real <bluesysugar4522@gmail.com>
2026-09-07 22:22:37 +0530

    Added a ExpenseService.java file, going to add implementation

---
+++ src/main/java/com/example/ExpenseSplitter/Services/ExpenseService.java
@@ -0,0 +1,15 @@
+package com.example.ExpenseSplitter.Services;
+
+import com.example.ExpenseSplitter.Models.Expense;
+import com.example.ExpenseSplitter.Models.PersonPaid;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class ExpenseService {
+    public List<PersonPaid> calculate(Expense e) {
+        // TODO - implement this shiiitttt
+
+        return null;
+    }
+}
